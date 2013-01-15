a9d5fe3745d16cecc8eb090a607751f1468b1652
Joel Plane <joel.plane@gmail.com>
2013-01-15 19:18:02 +1100

    clean up spec

--- spec/assert_type_spec.rb
+++ spec/assert_type_spec.rb
@@ -4,32 +4,38 @@ include AssertType::AssertMethods
 
 describe AssertType do
 
-  it "quick and nasty tests" do
-
-    at_assert_type Array, []
-    at_assert_type "Array", []
-    at_assert_type "Array", [1,2,3]
-    at_assert_type "Array<Fixnum>", [1,2,3]
-    at_assert_type "Array<Fixnum>", []
-    expect { at_assert_type "Array<Fixnum>", ["one", "two", "three"] }.to raise_error(AssertType::AssertionError) { |error|
-      error.message.should include %{expected Array<Fixnum> but was}
-    }
-    at_assert_type [Fixnum, String], 1
-    at_assert_type [Fixnum, String], "1"
-    expect { at_assert_type [Fixnum, String], 1.1 }.to raise_error(AssertType::AssertionError) { |error|
-      error.message.should include %{expected Fixnum or String but was 1.1}
-    }
-    at_assert_type "Array<Fixnum>, nil", [1,2,3]
-    at_assert_type "Array<Fixnum>, nil", []
-    at_assert_type "Array<Fixnum>, nil", nil
-    at_assert_type "String, Symbol", "hello"
-    expect { at_assert_type "Array<Fixnum>, nil", false }.to raise_error(AssertType::AssertionError)
-    expect { at_assert_type "Array<Fixnum>, nil", [nil] }.to raise_error(AssertType::AssertionError)
-
-    at_assert_type "String, Symbol", "hello"
-    at_assert_type "String, Symbol", :Symbol
-    expect { at_assert_type "String, Symbol", 42 }.to raise_error(AssertType::AssertionError)
-
+  def self.example_valid expected_type, value
+    it "should not raise when asserting #{value.inspect} is a #{expected_type}" do
+      at_assert_type expected_type, value
+    end
   end
+
+  def self.example_invalid expected_type, value
+    it "should raise when asserting #{value.inspect} is a #{expected_type}" do
+      expect { at_assert_type expected_type, value }.to raise_error(AssertType::AssertionError)
+    end
+  end
+
+  example_valid   Array, []
+  example_valid   "Array", []
+  example_valid   "Array", [1,2,3]
+
+  example_valid   "Array<Fixnum>", [1,2,3]
+  example_valid   "Array<Fixnum>", []
+  example_invalid "Array<Fixnum>", ["one", "two", "three"]
+
+  example_valid   [Fixnum, String], 1
+  example_valid   [Fixnum, String], "1"
+  example_invalid [Fixnum, String], 1.1
+
+  example_valid   "Array<Fixnum>, nil", [1,2,3]
+  example_valid   "Array<Fixnum>, nil", []
+  example_valid   "Array<Fixnum>, nil", nil
+  example_invalid "Array<Fixnum>, nil", false
+  example_invalid "Array<Fixnum>, nil", [nil]
+
+  example_valid   "String, Symbol", "hello"
+  example_valid   "String, Symbol", :Symbol
+  example_invalid "String, Symbol", 42
 
 end
